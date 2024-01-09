0c612cd33fe28467e2cb01285ca98ce0dc332151
Cirque0 <62349517+Cirque0@users.noreply.github.com>
2024-01-09 16:24:59 +0800

    display territories

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,99 @@
 "use client";
 
-import Image from "next/image";
-import { useEffect, useContext } from "react";
+import { useEffect, useContext, useMemo, useState } from "react";
 import { AuthContext } from "@/utils/AuthContext";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
     const router = useRouter();
     const { state, dispatch } = useContext(AuthContext);
+    const [locations, setLocations] = useState([]);
 
     useEffect(() => {
-        if(!state.user) {
-            router.push('/account/login')
+        if (!state.user) {
+            router.push("/account/login");
         }
-    }, [state.user])
+    }, [state.user]);
+
+    useEffect(() => {
+        const getLocations = async () => {
+            const res = await fetch(`${window.origin}/api/territories`);
+            const { data } = await res.json();
+
+            let sortedArr = [];
+            let mappedLocations = {};
+
+            data.forEach((location) => {
+                mappedLocations[location.id] = {
+                    ...location,
+                    children: [],
+                };
+            });
+
+            for (let id in mappedLocations) {
+                console.log(id);
+                const location = mappedLocations[id];
+                if (location.parent) {
+                    mappedLocations[location.parent].children.push(location);
+                } else {
+                    sortedArr.push(location);
+                }
+            }
+
+            setLocations(sortedArr);
+        };
+
+        getLocations();
+    }, []);
+
+    const logout = () => {
+        window.sessionStorage.removeItem("user");
+        dispatch({ type: "logout" });
+    };
 
     return (
-        <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-            {JSON.stringify(state.user)}
+        <main className='flex min-h-screen flex-col p-24 gap-4'>
+            {state.user ? (
+                <>
+                    <div className='flex items-center gap-4'>
+                        <p className='font-bold'>{state.user.displayName}</p>
+                        <button
+                            onClick={logout}
+                            className='w-fit bg-black text-white p-2 text-xs uppercase tracking-widest rounded'
+                        >
+                            Logout
+                        </button>
+                    </div>
+                    <p className='font-bold text-2xl'>Territories</p>
+                    <p>Here are the list of territories:</p>
+                    {locations.length ? (
+                        <ul className='list-disc main'>
+                            {locations.map((location) => (
+                                <Location location={location} />
+                            ))}
+                        </ul>
+                    ) : (
+                        <p>Wait a moment... </p>
+                    )}
+                </>
+            ) : (
+                <p className='font-bold text-xl'>
+                    You are not authenticated. Redirecting to login page...
+                </p>
+            )}
         </main>
+    );
+}
+
+function Location({ location }) {
+    return (
+        <li className='mx-8'>
+            <span>{location.name}</span>
+            <ul className='list-disc'>
+                {location.children.map((child) => (
+                    <Location location={child} />
+                ))}
+            </ul>
+        </li>
     );
 }
